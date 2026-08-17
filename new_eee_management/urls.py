9c1d653e634461a1f9c8e56fab4b019cd60152b1
Raju Ahmed <rajur20m@gmail.com>
2026-08-17 21:05:33 +0600

    update url for deploy

--- new_eee_management/urls.py
+++ new_eee_management/urls.py
@@ -1,14 +1,13 @@
 from django.contrib import admin
 from django.urls import path, include
-from django.shortcuts import redirect
+from eee.views import Userdashboard
 
 
 urlpatterns = [
+    path("", Userdashboard, name="root"),
+
     path("admin/", admin.site.urls),
 
     path("", include("user.urls")),
     path("", include("eee.urls")),
-
-    # Root URL → Public User Dashboard
-    path("", redirect("userdashboard")),
 ]
